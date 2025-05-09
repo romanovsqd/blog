0d4a1e11f7c7bb695feaebb5287fa698c97d1bff
Oleg Romanov <sqdcontrast@gmail.com>
2025-05-09 11:46:53 +0300

    feat(auth): implement user login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -29,6 +29,7 @@ class FortifyServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Fortify::registerView('auth.register');
+        Fortify::loginView('auth.login');
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title')
+    register
+@endsection
+
+@section('content')
+    <div>
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+
+            @if ($errors->any())
+                <div>
+                    <ul class="text-red-600">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <div>
+                <input type="email" name="email" placeholder="example@mail.com" class="border">
+            </div>
+
+            <div>
+                <input type="password" name="password" placeholder="password" class="border">
+            </div>
+
+            <button type="submit" class="p-2 border">Login</button>
+
+        </form>
+    </div>
+@endsection
