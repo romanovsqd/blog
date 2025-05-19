8b87645a28351f862c7a3500e32705bfda382684
Oleg Romanov <sqdcontrast@gmail.com>
2025-05-19 21:41:27 +0300

    refactor(requests): add auth check to Post and Category requests

--- app/Http/Requests/Admin/Category/CategoryRequest.php
+++ app/Http/Requests/Admin/Category/CategoryRequest.php
@@ -11,7 +11,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user()->isAdmin();
+        return auth()->check() && auth()->user()->isAdmin();
     }
 
     /**
--- app/Http/Requests/Admin/Post/PostRequest.php
+++ app/Http/Requests/Admin/Post/PostRequest.php
@@ -11,7 +11,7 @@ class PostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user()->isAdmin();
+        return auth()->check() && auth()->user()->isAdmin();
     }
 
     /**
