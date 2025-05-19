caa0ab302cdc43332caf8ed7058a624d5ef090e7
Oleg Romanov <sqdcontrast@gmail.com>
2025-05-19 13:46:06 +0300

    feat(categories): implement categories search

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class CategoryController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
-        $categories = Category::query()->paginate(10);
+        $request->validate([
+            'search' => ['nullable', 'string', 'max:255'],
+        ]);
+
+        $categories = Category::query()
+            ->when(
+                $request->search,
+                function (Builder $query, string $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                }
+            )
+            ->paginate(10)
+            ->withQueryString();
+
         return view('categories.index', compact('categories'));
     }
 
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -5,6 +5,18 @@
 @endsection
 
 @section('content')
+<div class="mb-2">
+        <form action="{{ route('admin.categories.index') }}" method="GET">
+
+            <div class="mb-2">
+                <input type="text" name="search" placeholder="search categories" class="border px-2 py-1">
+            </div>
+
+            <button type="submit" class="border px-2 py-1">search</button>
+
+        </form>
+    </div>
+
     <ul>
         @foreach ($categories as $category)
             <li class="mb-4">
