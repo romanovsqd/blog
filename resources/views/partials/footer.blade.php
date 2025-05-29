17572121055b724153c216ddfc4761cdbdff4de9
Oleg Romanov <sqdcontrast@gmail.com>
2025-05-29 11:45:57 +0300

    feat(ui): add footer component

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,5 @@
-<footer></footer>
+<footer class="bg-gray-100 p-4 text-center text-sm w-full">
+    © {{ date('Y') }}
+    <a href="https://github.com/romanovsqd" target="_blank" class="underline hover:text-">romanovsqd.</a>
+    All rights reserved. 💙
+</footer>
